383e6304dfcd1ddf06c7243fa583f6a15e663a8e
Phdoflip <antrodossantos@gmail.com>
2025-08-13 11:30:59 -0300

    certo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import "./App.css";
-
 export default function App() {
   return (
-    <footer className="footer">
-      <p>© 2025 Minha Empresa</p>
-      <ul>
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="#">LinkedIn</a></li>
-      </ul>
-    </footer>
+    <div className="page-container">
+      {/* Aqui pode entrar o conteúdo do site */}
+
+      <footer className="footer">
+        <p>© 2025 Minha Empresa</p>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">LinkedIn</a></li>
+        </ul>
+      </footer>
+    </div>
   );
 }
