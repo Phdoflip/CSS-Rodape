f9d43026a36efad0ca3ee276409bfd2e9b56dde6
Phdoflip <antrodossantos@gmail.com>
2025-08-13 11:28:18 -0300

    rodape inical

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Minha Empresa</p>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">LinkedIn</a></li>
+      </ul>
+    </footer>
+  );
+}
